efc220e7260128ebc8d69d506369f0eef8cea61b
7devanle <ffamexyz@gmail.com>
2024-01-07 17:34:29 +0100

    order item to methods

--- src/StoreClass.java
+++ src/StoreClass.java
@@ -1,9 +1,11 @@
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.Scanner;
 
 public class StoreClass {
     private static List<ProductForSale> marketItems = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
     static ProductForSale cowbell = new Milk("Cowbell", 23.5, "Milk from cowbell, sturdy and refreshing");
     static ProductForSale dana = new Milk("Dana", 20.0, "Milk from Dana company, sturdy but less refreshing");
     static ProductForSale tilapia = new Fish("Tilapia", 13.5, "Tilapia straight from the sea, delicious and expensive");
@@ -13,19 +15,37 @@ public class StoreClass {
 
     public static void main(String[] args) {
         marketItems.addAll(List.of(cowbell, dana, tilapia, panla, corn));
-
+        displayStoreItems();
+        orderItem();
     }
 
     public static void displayStoreItems(){
         int i = 1;
         for(ProductForSale product : marketItems){
             System.out.println(i + " "+ product );
+            i++;
         }
     }
 
-    static String orderItem(String type){
-
-        return "Item not avaialble";
+    static void orderItem(){
+        List<ProductForSale> orders = new ArrayList<>();
+        boolean shopping = true;
+        while(shopping){
+            System.out.println("Enter 0 to quit");
+            for(int i = 0; i<marketItems.size(); i++){
+                System.out.println("Enter "+(i+1)+" to order "+marketItems.get(i));
+            }
+            int input = scanner.nextInt();
+            if(input == 0){
+                shopping = false;
+                System.out.println("Done shopping");
+            }
+            else {
+                System.out.println(marketItems.get(input - 1));
+                orders.add(marketItems.get(input-1));
+            }
+        }
+        System.out.println(orders);
     }
 
     public static String addToStore(String product, String type, double price, String description){
